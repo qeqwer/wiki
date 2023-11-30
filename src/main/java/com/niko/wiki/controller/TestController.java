ca395fcbb2ec3d9583baedb0c03185a819723d4d
niko <1142521580@qq.com>
2023-11-30 21:42:55 +0800

    2-8 增加自定义配置项示例

--- src/main/java/com/niko/wiki/controller/TestController.java
+++ src/main/java/com/niko/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.niko.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Value("${test.hello:Test}")
+    private String testHello;
+
     @GetMapping("/hello")
     public String hello() {
-        return "Hello Word!";
+        return "Hello Word!" + testHello;
     }
 
     @PostMapping("/hello/post")
