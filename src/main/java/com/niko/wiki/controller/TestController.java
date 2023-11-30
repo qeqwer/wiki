177ab7aaf77ff9f5825d517db6753a34638a8967
niko <1142521580@qq.com>
2023-11-30 21:09:40 +0800

    2-7 增加hello接口测试test.http

--- src/main/java/com/niko/wiki/controller/TestController.java
+++ src/main/java/com/niko/wiki/controller/TestController.java
@@ -1,12 +1,19 @@
 package com.niko.wiki.controller;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestController{
+public class TestController {
 
-    @RequestMapping("/hello")
-    public String hello(){
-        return "hello";
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Word!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello Word! Post." + name;
     }
 }
