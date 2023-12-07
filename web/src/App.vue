7c0f9401cda789ae8c900b6e09f039d6bc872c3c
niko <1142521580@qq.com>
2023-12-07 23:19:27 +0800

    4-7 制作the-footer组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,20 +2,20 @@
   <a-layout id="components-layout-demo-top-side-2">
    <TheHeader></TheHeader>
     <router-view/>
-    <a-layout-footer style="text-align: center">
-      我的电子书
-    </a-layout-footer>
+   <TheFooterer></TheFooterer>
   </a-layout>
 </template>
 
 <script lang="ts">
   import {defineComponent} from 'vue';
   import TheHeader from "@/components/the-header.vue";
+  import TheFooter from "@/components/the-footer.vue";
 
   export default defineComponent({
     name: 'app',
     components:{
-      TheHeader
+      TheHeader,
+      TheFooter
     },
   });
 </script>
--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -53,14 +53,14 @@
   </a-layout>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+<!--<script lang="ts">-->
+<!--import { defineComponent } from 'vue';-->
+<!--import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src-->
 
-export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
-});
-</script>
+<!--export default defineComponent({-->
+<!--  name: 'HomeView',-->
+<!--  components: {-->
+<!--    HelloWorld,-->
+<!--  },-->
+<!--});-->
+<!--</script>-->
