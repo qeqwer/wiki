205a06b3574565fe282a8eb7f909da58506168a6
niko <1142521580@qq.com>
2024-12-19 23:06:02 +0800

    5-4 电子书布局调整 一行三列

--- src/main/java/com/niko/wiki/mapper/EbookMapper.java
+++ src/main/java/com/niko/wiki/mapper/EbookMapper.java
@@ -2,9 +2,8 @@ package com.niko.wiki.mapper;
 
 import com.niko.wiki.domain.Ebook;
 import com.niko.wiki.domain.EbookExample;
-import org.apache.ibatis.annotations.Param;
-
 import java.util.List;
+import org.apache.ibatis.annotations.Param;
 
 public interface EbookMapper {
     long countByExample(EbookExample example);
