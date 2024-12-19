42886a695e85e31c5f7098eb27347790a01df074
niko <1142521580@qq.com>
2024-12-19 20:48:07 +0800

    5-4 集成图表组件， npm install @ant-design/icons-vue --save

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -48,10 +48,38 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      <pre>
-{{ebooks}}
-{{ebooks2}}
-      </pre>
+      <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
+        <template #footer>
+          <div>
+            <b>ant design vue</b>
+            footer part
+          </div>
+        </template>
+        <template #renderItem="{ item }">
+          <a-list-item key="item.title">
+            <template #actions>
+          <span v-for="{ icon, text } in actions" :key="icon">
+            <component :is="icon" style="margin-right: 8px" />
+            {{ text }}
+          </span>
+            </template>
+            <template #extra>
+              <img
+                  width="272"
+                  alt="logo"
+                  src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+              />
+            </template>
+            <a-list-item-meta :description="item.description">
+              <template #title>
+                <a :href="item.href">{{ item.title }}</a>
+              </template>
+              <template #avatar><a-avatar :src="item.avatar" /></template>
+            </a-list-item-meta>
+            {{ item.content }}
+          </a-list-item>
+        </template>
+      </a-list>
 
     </a-layout-content>
   </a-layout>
@@ -60,6 +88,20 @@
 <script lang="ts">
 import {defineComponent, onMounted, ref, reactive, toRef} from 'vue';
 import axios from "axios";
+
+const listData: any = [];
+
+for (let i = 0; i < 23; i++) {
+  listData.push({
+    href: 'https://www.antdv.com/',
+    title: `ant design vue part ${i}`,
+    avatar: 'https://joeschmoe.io/api/v1/random',
+    description:
+        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    content:
+        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+  });
+}
 
 
 export default defineComponent({
@@ -82,7 +124,19 @@ export default defineComponent({
     });
     return {
       ebooks,
-      ebooks2:toRef(ebooks1,'books')
+      ebooks2:toRef(ebooks1,'books'),
+      listData,
+      pagination : {
+        onChange: (page:any) => {
+          console.log(page);
+        },
+        pageSize: 3,
+      },
+      actions:  [
+      { icon: 'StarOutlined', text: '156' },
+      { icon: 'LikeOutlined', text: '156' },
+      { icon: 'MessageOutlined', text: '2' },
+     ]
     };
   }
 
