cb59091e0c93ac25117ae8e7e87ee671dc57aeaf
niko <1142521580@qq.com>
2025-01-21 23:50:08 +0800

    8-3 1生成文档管理持久层代码     2完成文档管理前后端代码     3问题：编辑表单中无法选择二级文档做父文档

---
+++ src/main/java/com/niko/wiki/controller/DocController.java
@@ -0,0 +1,53 @@
+package com.niko.wiki.controller;
+
+
+import com.niko.wiki.req.DocQueryReq;
+import com.niko.wiki.req.DocSaveReq;
+import com.niko.wiki.resp.DocQueryResp;
+import com.niko.wiki.resp.CommonResp;
+import com.niko.wiki.resp.PageResp;
+import com.niko.wiki.service.DocService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/doc")
+public class DocController {
+
+    @Resource
+    private DocService docService;
+
+    @GetMapping("/all")
+    public CommonResp all() {
+        CommonResp<List<DocQueryResp>> resp = new CommonResp<>();
+        List<DocQueryResp> list = docService.all();
+        resp.setContent(list);
+        return resp;
+    }
+
+    @GetMapping("/list")
+    public CommonResp list(@Valid DocQueryReq req){
+        CommonResp<PageResp<DocQueryResp>> resp = new CommonResp<>();
+        PageResp<DocQueryResp> list = docService.list(req);
+        resp.setContent(list);
+        return resp;
+    }
+
+    @PostMapping("/save")
+    public CommonResp save(@Valid @RequestBody DocSaveReq req) {
+        CommonResp resp = new CommonResp<>();
+        docService.save(req);
+        return resp;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp delete(@PathVariable Long id) {
+        CommonResp resp = new CommonResp<>();
+        docService.delete(id);
+        return resp;
+    }
+
+}
