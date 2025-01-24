4cc6ccc906eea3a08b9a9e43ee763bffaa844568
niko <1142521580@qq.com>
2025-01-24 17:34:22 +0800

    8-5 增加页面参数传递完成新增功能 8-6 删除某个文档，将其下所有文档全部删除

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -93,11 +93,21 @@ import { defineComponent, onMounted, ref } from 'vue';
 import axios from 'axios';
 import {message} from "ant-design-vue";
 import {Tool} from "@/util/tool";
+import {useRoute} from "vue-router";
 
 
 export default defineComponent({
   name: 'AdminDoc',
   setup() {
+    const route = useRoute();
+    console.log("路由：", route);
+    console.log("route.path：", route.path);
+    console.log("route.query：", route.query);
+    console.log("route.param：", route.params);
+    console.log("route.fullPath：", route.fullPath);
+    console.log("route.name：", route.name);
+    console.log("route.meta：", route.meta);
+
     /**
      * 一级文档树，children属性就是二级文档
      * [{
@@ -189,6 +199,39 @@ export default defineComponent({
           }
         }
       }
+    }
+
+      const ids: Array<string> = [];
+
+      /**
+       * 查找整根树枝
+       */
+      const getDeleteIds = (treeSelectData: any, id: any) => {
+      // console.log(treeSelectData, id);
+      // 遍历数组，即遍历某一层节点
+      for (let i = 0; i < treeSelectData.length; i++) {
+        const node = treeSelectData[i];
+        if (node.id === id) {
+          // 如果当前节点就是目标节点
+          console.log("delete", node);
+          // 将目标节点放入Ids
+          ids.push(id);
+
+          // 遍历所有子节点
+          const children = node.children;
+          if (Tool.isNotEmpty(children)) {
+            for (let j = 0; j < children.length; j++) {
+              getDeleteIds(children, children[j].id)
+            }
+          }
+        } else {
+          // 如果当前节点不是目标节点，则到其子节点再找找看。
+          const children = node.children;
+          if (Tool.isNotEmpty(children)) {
+            getDeleteIds(children, id);
+          }
+        }
+      }
     };
 
 
@@ -229,7 +272,7 @@ export default defineComponent({
 
     const add = () =>{
       modalVisible.value = true;
-      doc.value = {};
+      doc.value = {ebookId: route.query.ebookId};
 
       treeSelectData.value = Tool.copy(level1.value);
 
@@ -238,7 +281,8 @@ export default defineComponent({
     };
 
     const handelDelete = (id: number) =>{
-      axios.delete("/doc/delete/" + id).then((response) => {
+      getDeleteIds(level1.value,id);
+      axios.delete("/doc/delete/" + ids.join(",")).then((response) => {
         const data = response.data;
         if(data.success){
           //重新加载列表
