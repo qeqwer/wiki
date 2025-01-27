bffac579dfe930ae1cc6b9ffa99c620563b41a65
niko <1142521580@qq.com>
2025-01-27 11:45:19 +0800

    8-12 1点击文档节点时，加载并显示文档内容      2打开文档内容增加Wangeditor样式，和富文本框保持一致      3文档内容和antdv p冲突，并覆盖文档内容显示样式

--- web/src/views/DocView.vue
+++ web/src/views/DocView.vue
@@ -8,13 +8,14 @@
           <a-tree
               v-if="level1.length > 0"
               :tree-data="level1"
-              @select="onselect"
+              @select="onSelect"
               :fieldNames="{title: 'name', key: 'id', value: 'id'}"
               :default-expand-all="true"
           >
           </a-tree>
         </a-col>
         <a-col :span="18">
+          <div class="wangeditor" v-html="html"></div>
         </a-col>
       </a-row>
     </a-layout-content>
@@ -33,6 +34,11 @@ export default defineComponent({
   setup() {
     const route = useRoute();
     const docs = ref();
+    const html = ref();
+
+    // 当前选中的文档
+    // const doc = ref();
+
     /**
      * 一级文档树，children属性就是二级文档
      * [{
@@ -51,20 +57,42 @@ export default defineComponent({
      * 数据查询
      **/
     const handleQuery = () => {
-      axios.get("/doc/all" + route.query.ebookId).then((response) => {
+      axios.get("/doc/all/" + route.query.ebookId).then((response) => {
         const data = response.data;
         if(data.success){
           docs.value = data.content;
-          console.log("原始数组：", docs.value);
 
           level1.value = [];
           level1.value = Tool.array2Tree(docs.value, 0);
-          console.log("树形结构：", level1);
         }
         else{
           message.error(data.message);
         }
       });
+    };
+
+    /**
+     * 内容查询
+     **/
+    const handleQueryContent = (id: number) => {
+      axios.get("/doc/find-content/" + id).then((response) => {
+        const data = response.data;
+        if (data.success) {
+          html.value = data.content;
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
+    const onSelect = (selectedKeys: any, info: any) => {
+      console.log('selected', selectedKeys, info);
+      if (Tool.isNotEmpty(selectedKeys)) {
+        // 选中某一节点时，加载该节点的文档信息
+        // doc.value = info.selectedNodes[0].props;
+        // 加载内容
+        handleQueryContent(selectedKeys[0]);
+      }
     };
 
     onMounted(() =>{
@@ -73,8 +101,84 @@ export default defineComponent({
 
     return{
       level1,
+      html,
+      onSelect,
     }
   }
 });
 </script>
 
+<style>
+  /* wangeditor默认样式, 参照: http://www.wangeditor.com/doc/pages/02-%E5%86%85%E5%AE%B9%E5%A4%84%E7%90%86/03-%E8%8E%B7%E5%8F%96html.html */
+  /* table 样式 */
+  .wangeditor table {
+    border-top: 1px solid #ccc;
+    border-left: 1px solid #ccc;
+  }
+  .wangeditor table td,
+  .wangeditor table th {
+    border-bottom: 1px solid #ccc;
+    border-right: 1px solid #ccc;
+    padding: 3px 5px;
+  }
+  .wangeditor table th {
+    border-bottom: 2px solid #ccc;
+    text-align: center;
+  }
+
+  /* blockquote 样式 */
+  .wangeditor blockquote {
+    display: block;
+    border-left: 8px solid #d0e5f2;
+    padding: 5px 10px;
+    margin: 10px 0;
+    line-height: 1.4;
+    font-size: 100%;
+    background-color: #f1f1f1;
+  }
+
+  /* code 样式 */
+  .wangeditor code {
+    display: inline-block;
+    *display: inline;
+    *zoom: 1;
+    background-color: #f1f1f1;
+    border-radius: 3px;
+    padding: 3px 5px;
+    margin: 0 3px;
+  }
+  .wangeditor pre code {
+    display: block;
+  }
+
+  /* ul ol 样式 */
+  .wangeditor ul, ol {
+    margin: 10px 0 10px 20px;
+  }
+
+  /* 和antdv p冲突，覆盖掉 */
+  .wangeditor blockquote p {
+    font-family:"YouYuan";
+    margin: 20px 10px !important;
+    font-size: 16px !important;
+    font-weight:600;
+  }
+
+  /*!* 点赞 *!*/
+  /*.vote-div {*/
+  /*  padding: 15px;*/
+  /*  text-align: center;*/
+  /*}*/
+
+  /* 图片自适应 */
+  .wangeditor img {
+    max-width: 100%;
+    height: auto;
+  }
+
+  /* 视频自适应 */
+  .wangeditor iframe {
+    width: 100%;
+    height: 400px;
+  }
+</style>
