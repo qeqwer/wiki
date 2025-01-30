6fbf58db18e9ba277c1f33ae439bafa81cebcfc0
niko <1142521580@qq.com>
2025-01-30 23:04:18 +0800

    9-2 用户表设计，生成持久层代码

--- doc/all.sql
+++ doc/all.sql
@@ -76,6 +76,17 @@ create table `content`(
 ) engine=innodb default charset=utf8mb4 comment='文档内容';
 
 
+drop table if exists `user`;
+create table `user`
+(
+    `id`   bigint not null comment 'id',
+    `login_name` varchar(50) not null comment '登录名',
+    `name` varchar(50) comment '昵称',
+    `password` varchar(32) not null comment '密码',
+    primary key (`id`),
+    unique key `login_name_unique` (`login_name`)
+)
+    engine=innodb default charset=utf8mb4 comment='用户';
 
 
 drop table if exists `test`;
