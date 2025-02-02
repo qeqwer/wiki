f067a194f54434717abf7b36225bb7d50e585a85
niko <1142521580@qq.com>
2025-02-03 00:01:55 +0800

    9-4 增加用户名不可重复，增加自定义异常

--- src/main/java/com/niko/wiki/service/UserService.java
+++ src/main/java/com/niko/wiki/service/UserService.java
@@ -4,6 +4,8 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.niko.wiki.domain.User;
 import com.niko.wiki.domain.UserExample;
+import com.niko.wiki.exception.BusinessException;
+import com.niko.wiki.exception.BusinessExceptionCode;
 import com.niko.wiki.mapper.UserMapper;
 import com.niko.wiki.req.UserQueryReq;
 import com.niko.wiki.req.UserSaveReq;
@@ -14,9 +16,11 @@ import com.niko.wiki.utils.SnowFlake;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
+import java.util.Collections;
 import java.util.List;
 
 
@@ -63,12 +67,30 @@ public class UserService {
     public void save(UserSaveReq req) {
         User user = CopyUtil.copy(req, User.class);
         if (ObjectUtils.isEmpty(req.getId())) {
-            // 新增
-            user.setId(snowFlake.nextId());
-            userMapper.insert(user);
+            User userDB = selectByLoginName(req.getLoginName());
+            if(ObjectUtils.isEmpty(userDB)) {
+                // 新增
+                user.setId(snowFlake.nextId());
+                userMapper.insert(user);
+            }else{
+                // 用户名已存在
+                throw new BusinessException(BusinessExceptionCode.USER_LOGIN_NAME_EXIST);
+            }
         } else {
             // 更新
             userMapper.updateByPrimaryKey(user);
+        }
+    }
+
+    public User selectByLoginName(String loginName){
+        UserExample userExample = new UserExample();
+        UserExample.Criteria criteria = userExample.createCriteria();
+        criteria.andNameLike( loginName);
+        List<User> userList = userMapper.selectByExample(userExample);
+        if(CollectionUtils.isEmpty(userList)){
+            return null;
+        }else {
+            return userList.get(0);
         }
     }
 
