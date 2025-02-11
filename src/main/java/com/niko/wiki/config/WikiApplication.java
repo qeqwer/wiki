353e7e4abdb69b6c286f1d4c1e422e3ac34a7627
niko <1142521580@qq.com>
2025-02-11 11:13:40 +0800

    10-4 电子书信息实时更新

--- src/main/java/com/niko/wiki/config/WikiApplication.java
+++ src/main/java/com/niko/wiki/config/WikiApplication.java
@@ -7,11 +7,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
 @ComponentScan("com.niko")
 @MapperScan("com.niko.wiki.mapper")
+@EnableScheduling
 public class WikiApplication {
 	private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
 
--- src/main/java/com/niko/wiki/mapper/DocMapperCust.java
+++ src/main/java/com/niko/wiki/mapper/DocMapperCust.java
@@ -11,4 +11,6 @@ public interface DocMapperCust {
 
     public void increaseVoteCount(@Param("id") Long id);
 
+    public void updateEbookInfo();
+
 }
